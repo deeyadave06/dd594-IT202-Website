5fab6be1a3f4f288f1eab50ed45c7b8f2b4d292a
Deeya Dave <deeyadave06@gmail.com>
2025-09-20 13:55:45 -0400

    Created CoffeeManagersStatements.sql

---
+++ scripts/CoffeeManagersStatements.sql
@@ -0,0 +1,21 @@
+SHOW DATABASES;
+USE dd594;
+CREATE TABLE CoffeeManagers (
+ CoffeeManagerID  INT(11)        NOT NULL   AUTO_INCREMENT,
+ emailAddress        VARCHAR(255)   NOT NULL   UNIQUE,
+ password            VARCHAR(64)    NOT NULL,
+ pronouns            VARCHAR(60)    NOT NULL,
+ firstName           VARCHAR(60)    NOT NULL,
+ lastName            VARCHAR(60)    NOT NULL,
+ DateTimeCreated     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+ DateTimeUpdated     TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+ PRIMARY KEY (CoffeeManagerID)
+);
+SHOW TABLES;
+SHOW CREATE TABLE CoffeeManagers;
+DESCRIBE CoffeeManagers;
+INSERT INTO CoffeeManagers (emailAddress, password, pronouns, firstName, lastName)
+VALUES
+('john@coffee.com', SHA2('StrongP@ssword!', 256), 'He/Him', 'John', 'Doe'),
+('jane@coffee.com', SHA2('L0ngPass123', 256), 'She/Her', 'Jane', 'Doe'),
+('deeya@coffee.com', SHA2('myL0ngP@ssword', 256), 'She/Her', 'Deeya', 'Dave');
